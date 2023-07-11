f0dcbdf58a393aee5be549f37f78544d0252d10e
Kagami Sascha Rosylight <saschanaz@outlook.com>
2023-07-11 19:47:45 +0200

    refactor(webpack): stop using path.dirname

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,10 @@
 import { fileURLToPath } from "url";
-import path from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default {
   mode: "production",
   entry: "./lib/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: fileURLToPath(new URL("./dist", import.meta.url)),
     filename: "index.cjs",
     libraryTarget: "commonjs2",
   },
